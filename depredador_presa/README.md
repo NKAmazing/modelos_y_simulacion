5656248533e63ae23c576903623b9e69de328023
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2024-03-27 00:40:14 -0300

    README added

---
+++ depredador_presa/README.md
@@ -0,0 +1,39 @@
+# Simulación de Modelo Depredador Presa
+
+Esta es una simulación de poblaciones de liebres y zorros en un campo cerrado natural, donde no intervienen otros animales ni predadores, con el objetivo de estudiar la manera en la que interactuan ambas poblaciones en un determinado tiempo.
+
+## Instalación
+
+1. Clonar el repositorio:
+
+```sh
+git clone https://github.com/NKAmazing/modelos_y_simulacion.git
+```
+
+2. Entrar a la carpeta:
+
+```sh
+cd depredador_presa
+```
+
+3. Crear el entorno virtual:
+
+```sh
+python3 -m venv .
+```
+
+4. Activar el entorno virtual:
+
+```sh
+cd .\Scripts\
+
+activate
+
+cd ..
+```
+
+5. Instalar los requerimientos:
+
+```sh
+pip install -r requirements.txt
+```
--- depredador_presa/predator_prey.py
+++ depredador_presa/predator_prey.py
@@ -25,15 +25,12 @@ class PredatorPrey():
 
         time = initial_time
 
-        for i in range(initial_time, 10):
+        for i in range(initial_time, 5):
             time = time + dt
             preys_loss_rate_after_hunt, predators_growth_rate_after_hunt, hunt_encounter_value = self.hunt_event(preys_list[-1], predators_list[-1], predators_death_rate)
             actual_capacity = self.get_actual_capacity(preys_list[-1], self.land_capacity)
             preys_updated = self.get_variations_preys(preys_list[-1], preys_loss_rate_after_hunt, dt, self.land_capacity, actual_capacity, preys_birth_rate, hunt_encounter_value)
             predators_updated = self.get_variations_predators(predators_list[-1], dt, predators_death_rate, hunt_encounter_value, predators_growth_rate_after_hunt)
-            
-            print("Predators updated: ", predators_updated)
-
             preys_list.append(preys_updated)
             predators_list.append(predators_updated)
             time_list.append(time)
@@ -45,14 +42,9 @@ class PredatorPrey():
         '''
         Calculate the number of preys and predators after the hunt event
         '''
-        print("Actual preys del HUNT EVENT: ", actual_preys)
-        print("Actual predators del HUNT EVENT: ", actual_predators)
         hunt_encounter_value = actual_preys * actual_predators
-        print("Hunt encounter value: ", hunt_encounter_value)
         predators_loss = predators_death_rate * actual_predators
-        print("Predators loss del HUNT EVENT: ", predators_loss)
         predators_growth_rate_after_hunt = predators_loss / hunt_encounter_value
-        print("Predators growth rate after hunt del HUNT EVENT: ", predators_growth_rate_after_hunt)
         preys_loss_rate_after_hunt = actual_predators / hunt_encounter_value
         return preys_loss_rate_after_hunt, predators_growth_rate_after_hunt, hunt_encounter_value
     
@@ -71,14 +63,8 @@ class PredatorPrey():
         '''
         Calculate the number of predators
         '''
-        print("Predators: ", actual_predators)
-        print("Time variation: ", dt)
-        print("Predators death rate: ", predators_death_rate)
-        print("Predators growth rate after hunt: ", predators_growth_rate_after_hunt)
         predators_survival = actual_predators * predators_death_rate
-        print("Predators survival: ", predators_survival)
-        predators_variation = actual_predators + dt * (predators_growth_rate_after_hunt * hunt_encounter_value - predators_survival)
-        print("Predators variation: ", predators_variation)
+        predators_variation = actual_predators + dt * ((predators_growth_rate_after_hunt * hunt_encounter_value) - predators_survival)
         return predators_variation
     
     def get_actual_capacity(self, actual_preys, land_capacity):
